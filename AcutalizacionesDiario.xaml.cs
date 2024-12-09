8b5a7a4324737a45177dd6aa6e39abc4a2ceae36
AnthonnyM31 <anthonnymosquera1492@gmail.com>
2024-12-08 22:04:13 -0500

    creamos todos los modelos

---
+++ AcutalizacionesDiario.xaml.cs
@@ -0,0 +1,9 @@
+namespace MosqueraAnthonny_ProyectoAvance2;
+
+public partial class AcutalizacionesDiario : ContentPage
+{
+	public AcutalizacionesDiario()
+	{
+		InitializeComponent();
+	}
+}
---
+++ VerDiario.xaml.cs
@@ -0,0 +1,9 @@
+namespace MosqueraAnthonny_ProyectoAvance2;
+
+public partial class VerDiario : ContentPage
+{
+	public VerDiario()
+	{
+		InitializeComponent();
+	}
+}
