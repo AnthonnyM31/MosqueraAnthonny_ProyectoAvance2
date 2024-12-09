b1166cbd2c1b77bf496817ef0e3e31d1e8374155
AnthonnyM31 <anthonnymosquera1492@gmail.com>
2024-12-08 21:52:51 -0500

    creación proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MosqueraAnthonny_ProyectoAvance2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MosqueraAnthonny_ProyectoAvance2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
